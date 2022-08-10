213406de71d0f6939b076b817037b46d70e3fe5e
hee <jkgt0521@naver.com>
2022-08-10 10:27:18 +0900

    Feat: #5 쉼터 목록 ListView

--- hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
+++ hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
@@ -9,17 +9,8 @@ import com.hygge.hygge.shelter_list.ShelterListCustomAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    val shelterList = arrayListOf<Shelter>(
-        Shelter("쉼터1", "안녕하숑", "tmp1"),
-        Shelter("쉼터2", "반가워요", "tmp_image")
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.shelter_list_view)
-
-        val customAdapter = ShelterListCustomAdapter(this, shelterList)
-        val listView = findViewById<ListView>(R.id.shelter_list_view)
-        listView.adapter = customAdapter
+        setContentView(R.layout.activity_main)
     }
 }
