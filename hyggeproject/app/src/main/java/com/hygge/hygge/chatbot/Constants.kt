69e255d9957daa7ad14f157bd15fe3dc93cfa20a
seeunjang <wisjang1@naver.com>
2022-08-29 14:39:08 +0900

    Feat:#6 검색필터링 커스텀 모달, 챗봇 화면 구성

---
+++ hyggeproject/app/src/main/java/com/hygge/hygge/chatbot/Constants.kt
@@ -0,0 +1,8 @@
+package com.hygge.hygge.chatbot
+
+object Constants {
+
+    const val SEND_ID = "SEND_ID"
+    const val RECEIVE_ID = "RECEIVE_ID"
+
+}
---
+++ hyggeproject/app/src/main/java/com/hygge/hygge/chatbot/Message.kt
@@ -0,0 +1,3 @@
+package com.hygge.hygge.chatbot
+
+data class Message(val message: String, val id: String, val time: String)
