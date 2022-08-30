69939ffddccc9018a4ef817bdd25764e4fcbc57e
yubin <garmalhy1@gmail.com>
2022-08-30 14:53:54 +0900

    Feat: #2 메인 화면 구성하기

--- hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
+++ hyggeproject/app/src/main/java/com/hygge/hygge/MainActivity.kt
@@ -2,15 +2,40 @@ package com.hygge.hygge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ListView
-import com.hygge.hygge.shelter_list.Shelter
-import com.hygge.hygge.shelter_list.ShelterListCustomAdapter
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var personList: List<Person>
+    private lateinit var adapter: ExpandableAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_list)
+
+        personList = ArrayList()
+        personList = loadData()
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ExpandableAdapter(personList)
+        recyclerView.adapter = adapter
+
+    }
+
+    private fun loadData(): List<Person> {
+        val people = ArrayList<Person>()
+
+        val persons = resources.getStringArray(R.array.people)
+
+        for (i in persons.indices) {
+            val person = Person().apply {
+                name = persons[i]
+            }
+            people.add(person)
+        }
+        return people
     }
 }
